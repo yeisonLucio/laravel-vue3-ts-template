27a61522e065f955565a730cfb8d156fa2493014
Yeison Lucio <yeisonlucio1266@gmail.com>
2023-06-06 12:55:05 -0500

    feat: Agrega vue y vuetify para el frontend

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,41 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
     plugins: [
+        vue(
+            {
+                template: { transformAssetUrls }
+            }
+        ),
+        vuetify({
+            autoImport: true,
+        }),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/ts/main.ts'
+            ],
             refresh: true,
         }),
     ],
+
+    define: { 'process.env': {} },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/ts', import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
 });
